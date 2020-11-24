fe7983405cd06e9f45bfeda52ba38a539b22ac01
Andres Espejo <jeaespejogo@unal.edu.co>
2020-11-24 13:36:08 -0500

    Configuration of portfolio section

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,3 +63,25 @@ function scrollTop() {
   else scrollTop.classList.remove('show-scroll');
 }
 window.addEventListener('scroll', scrollTop);
+
+/**Configuration mixit library */
+var mixer = mixitup('.portfolio__container', {
+  selectors: {
+    target: '.portfolio__content',
+  },
+  animation: {
+    duration: 400,
+  },
+});
+
+/**Link active portfolio */
+
+const linkPortfolio = document.querySelectorAll('.portfolio__item');
+
+function activePortfolio() {
+  if (linkPortfolio) {
+    linkPortfolio.forEach((l) => l.classList.remove('active-portfolio'));
+    this.classList.add('active-portfolio');
+  }
+}
+linkPortfolio.forEach((l) => l.addEventListener('click', activePortfolio));
